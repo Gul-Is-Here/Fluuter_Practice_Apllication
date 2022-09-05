e5dd150b58f566017baee1d39693a90143b79e62
Gul Faraz Ahmed <gulfarazahmed58@gmail.com>
2022-09-05 08:41:38 -0700

    chapter2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,38 @@ class MyApp extends StatelessWidget{
     return Scaffold(appBar: AppBar(
       title: Text (' My App'),
     ),
-    body: Container(child:
-     Center(child:
-      Text(
-        ' Hi Flutter'
-        ),),),
+    body: Align(
+      child: Container(
+        color: Color.fromARGB(219, 220, 180, 59),
+          width: MediaQuery.of(context).size.width,
+           height:MediaQuery.of(context).size.height/2,
+        child: Column(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: [
+            Container(
+             padding: EdgeInsets.all(15),
+           width: 100,
+           height: 100,
+             color: Colors.green,
+            ),
+             Container(
+             padding: EdgeInsets.all(15),
+             width: 100,
+             height: 100,
+             color: Colors.yellow,
+            ),
+             Container(
+             padding: EdgeInsets.all(15),
+             width: 100,
+             height: 100,
+             color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    ),
     );
   }
 }
